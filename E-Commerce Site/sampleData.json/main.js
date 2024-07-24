2fb1192012c910f03fa61e09a47a6b8ac735f51c
sayed-qubadi <>
2024-07-24 16:23:37 -0700

    initial data entry
    JSON file: four items added

---
+++ E-Commerce Site/sampleData.json/main.js
@@ -0,0 +1,37 @@
+[
+    {
+        "name": "trousers",
+        "color": "black",
+        "gender": "neutral",
+        "price": 60,
+        "in-stock": true,
+        "item no.": 1
+    }
+    {
+        "name": "blouse",
+        "color": "pink",
+        "gender": "women",
+        "price": 65,
+        "in-stock": true,
+        "item no.": 2
+
+    }
+    {
+        "name": "skirt",
+        "color": "red",
+        "gender": "women",
+        "price": 45,
+        "in-stock": true,
+        "item no.": 3
+
+    }
+    {
+        "name": "hoodie",
+        "color": "green",
+        "gender": "men",
+        "price": 50,
+        "in-stock": true,
+        "item no.": 4
+
+    }
+    ]
