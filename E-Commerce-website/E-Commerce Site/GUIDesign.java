18df061016658dc2d29428060c5189016d9cc16f
Jackrayallday <jackray3111@gmail.com>
2024-07-24 17:26:31 -0700

    Delete E-Commerce-website/E-Commerce Site directory
    TO clear space for new version that works

--- E-Commerce-website/E-Commerce Site/GUIDesign.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.*;
-
-public class GUIDesign {
-    public static void main(String[] args) {
-        new CreatePages();
-    }
-}
--- E-Commerce-website/E-Commerce Site/siteLogic.java
+++
@@ -1,8 +0,0 @@
-
-
-
-// will use this file for the main logic behind how everything works //
-//such as the logic behind buttons, different pages doing different things etc. //
-public class siteLogic {
-    
-}
