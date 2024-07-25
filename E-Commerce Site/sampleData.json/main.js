b66efd876ff140e8284c3c2f5bba3764c5659a76
sayed-qubadi <>
2024-07-24 17:01:17 -0700

    Update main.js
    added commas between items, more items

--- E-Commerce Site/sampleData.json/main.js
+++ E-Commerce Site/sampleData.json/main.js
@@ -2,36 +2,65 @@
     {
         "name": "trousers",
         "color": "black",
-        "gender": "neutral",
+        "gender": "unisex",
+        "size": "large",
         "price": 60,
         "in-stock": true,
         "item no.": 1
-    }
+    },
     {
         "name": "blouse",
         "color": "pink",
         "gender": "women",
+        "size": "medium",
         "price": 65,
         "in-stock": true,
         "item no.": 2
 
-    }
+    },
     {
         "name": "skirt",
         "color": "red",
         "gender": "women",
+        "size": "small",
         "price": 45,
         "in-stock": true,
         "item no.": 3
 
-    }
+    },
     {
         "name": "hoodie",
         "color": "green",
         "gender": "men",
+        "size": "large",
         "price": 50,
         "in-stock": true,
         "item no.": 4
 
+    },
+    {
+        "name": "pants",
+        "color": "black",
+        "gender": "men",
+        "size": "medium",
+        "price": 85,
+        "in-stock": true,
+        "item no.": 5
+    },
+    {
+        "name": "jeans",
+        "color": "blue",
+        "gender": "unisex",
+        "price": 70,
+        "in-stock": true,
+        "item no.": 6
+    },
+    {
+        "name": "windbreaker",
+        "color": "green",
+        "gender": "men",
+        "price": 95,
+        "in-stock": true,
+        "item no.": 7
     }
     ]
