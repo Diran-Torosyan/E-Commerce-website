63af77d0ef368c95b92f8ed7abe8cd8b299a0839
Jackrayallday <jackray3111@gmail.com>
2024-08-07 15:41:07 -0700

    Add files via upload

---
+++ mavenConversion/my-app/src/main/java/com/example/GUIDesign.java
@@ -0,0 +1,11 @@
+package com.example;
+import javax.swing.SwingUtilities;
+
+public class GUIDesign {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            CreatePages var0 = new CreatePages();
+            var0.homeDisplay();
+        });
+    }
+}
