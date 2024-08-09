22e796d737c354f513ab0ba648d84b30280301b6
Jackrayallday <jackray3111@gmail.com>
2024-08-09 05:44:13 -0700

    Update User.java
    Fixed the import statement issue after putting the in class changes in

--- E-Commerce-website/mavenConversion/my-app/src/main/java/com/example/User.java
+++ E-Commerce-website/mavenConversion/my-app/src/main/java/com/example/User.java
@@ -9,6 +9,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 public class User {
 
