82cb6d93904d80c87a86868fc84ee47ff074b78d
sayed-qubadi <>
2024-08-13 15:05:50 -0700

    CreatePages commented

--- E-Commerce-website/E-Commerce-website/mavenConversion/my-app/src/main/java/com/example/CreatePages.java
+++ E-Commerce-website/E-Commerce-website/mavenConversion/my-app/src/main/java/com/example/CreatePages.java
@@ -38,6 +38,18 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class demonstrates the main GUI design for the e-commerce application with product showcase, filters, 
+ * search, cart, checkout, and user login/signup functionalities.
+ * 
+ * <p>The CreatePages class extends JFrame class in the Java Swing library to access its components. It formats an
+ * interactive main page with the products' images. It also includes check boxes to apply filters, a search bar to 
+ * filter and display products based on String input, and buttons to view cart, checkout, and login/signup. 
+ * 
+ * @author Jack Ray
+ * @author Diran Torosyan
+ * @since 1.0 (2024-07-17)
+ */
 public class CreatePages extends JFrame {
     private JTextField searchBar;
     private JButton searchButton;
@@ -48,14 +60,24 @@ public class CreatePages extends JFrame {
 
     private JCheckBox menFilter;
     private JCheckBox womenFilter;
-
+    /**
+     * Constructs a new e-commerce application main page. It creates a Cart object, and calls initializeProducts(), 
+     * homeDisplay(), and itemDisplay() with products HashMap.
+     * @see Cart
+     */
     public CreatePages() {
         cart = new Cart();
         initializeProducts();
         homeDisplay();
         itemDisplay(products);
     }
-
+    /**
+     * Creates a hash map. Initializes products in the input file with ID, then saves them as instances of Item class in 
+     * in the hash map created previously.
+     * 
+     * @see Item
+     * @exception printStackTrace input file cannot be accessed
+     */
     private void initializeProducts() {
         products = new HashMap<>();
         try {
@@ -70,7 +92,9 @@ public class CreatePages extends JFrame {
             e.printStackTrace();
         }
     }
-
+    /**
+     * Formats the home page; title, color, button/bar arrangments, and other GUI features.
+     */
     void homeDisplay() {
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -78,62 +102,84 @@ public class CreatePages extends JFrame {
         setSize(screenSize);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
-
+        /**
+         * Panel for home page
+         */
         JPanel bar = new JPanel();
         bar.setPreferredSize(new Dimension(screenSize.width, 50));
         bar.setBackground(Color.WHITE);
         bar.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
-
+        /**
+         * 'Home' page indicator
+         */
         JLabel home = new JLabel("Home");
         home.setForeground(Color.GRAY);
         home.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.BLACK, Color.GRAY));
         home.setFocusable(true);
-
+        /**
+         * search bar
+         */
         searchBar = new JTextField();
         searchBar.setPreferredSize(new Dimension(300, 30));
         searchBar.setFocusable(true);
-
+        /**
+         * search button
+         */
         searchButton = new JButton("search");
         searchButton.setPreferredSize(new Dimension(100, 30));
         searchButton.addActionListener(e -> search());
-
+        /**
+         * Check boxes to apply filters 
+         */
         menFilter = new JCheckBox("Men's");
         womenFilter = new JCheckBox("Women's");
         menFilter.addActionListener(e -> applyFilters());
         womenFilter.addActionListener(e -> applyFilters());
-
+        /**
+         * panel to make space
+         */
         JLabel space = new JLabel();
         space.setPreferredSize(new Dimension(300, 30));
         space.setFocusable(true);
-
+        /**
+         * Cart button to view cart
+         */
         JButton cartButton = new JButton("Cart");
         cartButton.setForeground(Color.GRAY);
         cartButton.setPreferredSize(new Dimension(80, 30));
         cartButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.BLACK, Color.GRAY));
         cartButton.setFocusable(true);
         cartButton.addActionListener(e -> viewCart());
-
+        /**
+         * Checkout button to initiate checkout
+         */
         JButton checkoutButton = new JButton("Checkout");
         checkoutButton.setForeground(Color.GRAY);
         checkoutButton.setPreferredSize(new Dimension(100, 30));
         checkoutButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.BLACK, Color.GRAY));
         checkoutButton.setFocusable(true);
         checkoutButton.addActionListener(e -> checkout());
-
+        /**
+         * Login button to initiate user login
+         */
         JButton loginButton = new JButton("Login");
         loginButton.setForeground(Color.GRAY);
         loginButton.setPreferredSize(new Dimension(80, 30));
         loginButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.BLACK, Color.GRAY));
         loginButton.setFocusable(true);
         loginButton.addActionListener(e -> login());
-
+        /**
+         * Logout button to initiate user logout 
+         */
         JButton logoutButton = new JButton("Logout");
         logoutButton.setForeground(Color.GRAY);
         logoutButton.setPreferredSize(new Dimension(80, 30));
         logoutButton.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, Color.BLACK, Color.GRAY));
         logoutButton.setFocusable(true);
         logoutButton.addActionListener(e -> logout());
-
+        /**
+         * add all GUI's created above to the 'bar' panel 
+         */
         add(bar);
         bar.add(home);
         bar.add(searchBar);
@@ -145,11 +191,15 @@ public class CreatePages extends JFrame {
         bar.add(checkoutButton);
         bar.add(loginButton);
         bar.add(logoutButton);
-
+        /**
+         * Create a panel with a gridded layout for items display
+         */
         add(bar, BorderLayout.NORTH);
         items = new JPanel();
         items.setLayout(new GridLayout(0, 4, 10, 10));
-
+        /**
+         * scroll pane
+         */
         scrollPane = new JScrollPane(items);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
@@ -159,7 +209,10 @@ public class CreatePages extends JFrame {
         setResizable(false);
         setVisible(true);
     }
-
+    /**
+     * 
+     * @param productsToDisplay
+     */
     private void itemDisplay(Map<Integer, Item> productsToDisplay) {
         items.removeAll();
 
@@ -203,12 +256,18 @@ public class CreatePages extends JFrame {
         items.revalidate();
         items.repaint();
     }
-
+    /**
+     * Initiates search and filters the results
+     */
     private void search() {
         Map<Integer, Item> searchedProducts = searchProducts();
         filterProducts(searchedProducts);
     }
-
+    /**
+     * Maps all items for user string input, then puts the results in newly created hash map searchedProducts.
+     * Case-insensitive
+     * @return the search results as a map
+     */
     private Map<Integer, Item> searchProducts() {
         String query = searchBar.getText().toLowerCase();
         Map<Integer, Item> searchedProducts = new HashMap<>();
@@ -221,7 +280,11 @@ public class CreatePages extends JFrame {
 
         return searchedProducts;
     }
-
+    /**
+     * Initiates filtering
+     * 
+     * @param productsToFilter indicates the factor based on which the items are filtered; "Men's" or "Women's"
+     */
     private void filterProducts(Map<Integer, Item> productsToFilter) {
         if (menFilter.isSelected()) {
             productsToFilter.values().removeIf(item -> !item.getCategory().equalsIgnoreCase("Men's"));
@@ -233,12 +296,16 @@ public class CreatePages extends JFrame {
 
         itemDisplay(productsToFilter);
     }
-
+    /**
+     * Applys filter and displays the results 
+     */
     private void applyFilters() {
         Map<Integer, Item> searchedProducts = searchProducts();
         filterProducts(searchedProducts);
     }
-
+    /**
+     * Creates panel with a 'box' layout to display items in current cart; panel is titled "Your Cart"
+     */
     private void viewCart() {
         JPanel cartPanel = new JPanel();
         cartPanel.setLayout(new BoxLayout(cartPanel, BoxLayout.Y_AXIS));
@@ -249,12 +316,17 @@ public class CreatePages extends JFrame {
 
         JOptionPane.showMessageDialog(this, cartPanel, "Your Cart", JOptionPane.INFORMATION_MESSAGE);
     }
-
+    /**
+     * To checkout items in cart; creates an instance of CheckoutPage with current cart, and calls method sendEmailReceipt()
+     * @see CheckoutPage
+     */
     private void checkout() {
         new CheckoutPage(cart);
         sendEmailReceipt();
     }
-
+    /**
+     * Processes sending receipt through email; authentication, preparation, and exception
+     */
     private void sendEmailReceipt() {
         String to = "jackray3111@gmail.com";
         String from = "generalclothingsupply@yahoo.com";
@@ -293,16 +365,23 @@ public class CreatePages extends JFrame {
             JOptionPane.showMessageDialog(this, "Failed to send email: " + mex.getMessage());
         }
     }
-
+    /**
+     * creates and displays a new loginPage object
+     * @see loginPage
+     */
     private void login() {
         loginPage frame = new loginPage();
         frame.setVisible(true);
     }
-
+    /**
+     * When implemented: logs user out, then creates and displays a new CreatePages object
+     */
     private void logout() {
         JOptionPane.showMessageDialog(this, "Logout functionality to be implemented");
     }
-
+    /**
+     * The execution point of CreatePages
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(CreatePages::new);
     }
