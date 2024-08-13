6cd36b136971b9ed9f9f263579dc2a14db58dd5a
Diran Torosyan <122069583+Diran-Torosyan@users.noreply.github.com>
2024-08-12 17:04:25 -0700

    Delete E-Commerce-website/E-Commerce-website/mavenConversion/my-app directory

--- E-Commerce-website/E-Commerce-website/mavenConversion/my-app/src/main/java/com/example/siteLogic.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-
-// will use this file for the main logic behind how everything works //
-//such as the logic behind buttons, different pages doing different things etc. //
-public class siteLogic {
-    
-}
